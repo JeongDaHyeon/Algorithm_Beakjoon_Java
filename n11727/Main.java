e85feb7e9d8e5d1abf31b7f7d52ea780074f1c4f
JeongDaHyeon <201802152@o.cnu.ac.kr>
2019-12-29 21:18:43 +0900

    11727_2xn 타일링 2

--- n11727/Main.java
+++ n11727/Main.java
@@ -12,7 +12,14 @@ public class Main {
 		
 		int[] dp = new int[n+1];
 		dp[0] = 1;
+		dp[1] = 1;
 		
+		for(int i = 2; i <= n; i++) {
+			dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
+		}
+		
+		
+		/*
 		for(int i = 0; i < n; i++) {
 			if(i + 1 <= n) {
 				dp[i+1] += dp[i];
@@ -23,6 +30,8 @@ public class Main {
 				dp[i+1] %= 10007;
 			}
 		}
+		*/
+		
 				
 		System.out.println(dp[n]);
 
